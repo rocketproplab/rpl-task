59091f30d94f9d7973cbb0792aedaf0172b3dff7
Combobyte <varun.sreedhar1@gmail.com>
2023-01-14 10:39:44 -0800

    finished commenting linkedlist

--- TaskLinkedList.h
+++ TaskLinkedList.h
@@ -8,15 +8,53 @@ public:
 		RplTask* value;
 		Node() : next(nullptr), value(nullptr){}
 	};
+	
+	/**
+	* Returns the size of the linked list
+	*/
 	int getSize();
+
+
+	/**
+	* adds the task to the end of the linked list
+	*/
 	void add(RplTask* obj);
+
+
+	/**
+	* Returns the element at specified index
+	*/
 	RplTask* get(int index);
+
+
+	/**
+	* Returns true if element is in linkedlist
+	*/
 	bool contains(RplTask* task);
+
+
+	/**
+	* Gets the index of an element (-1) if doesn't exist
+	*/
 	int getIndex(RplTask* task);
+
+
+	/**
+	* Deletes element from linked list if it exists
+	*/
 	void deleteTask(RplTask* task);
-	TaskLinkedList() : size(0), head(nullptr), tail(nullptr) {};
-	int size;
+
+	/**
+	* Helper function clear memory
+	*/
 	void deleteNode(Node* node);
+
+	/**
+	* Constructor for empty linked list
+	*/
+	TaskLinkedList() : size(0), head(nullptr), tail(nullptr) {};
+
+	int size;
 	Node* head;
 	Node* tail;
 	~TaskLinkedList();
