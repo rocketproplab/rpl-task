116e456aa3f33884dfdc348394f576282797f0db
Sean Lipps <slipps@ucsd.edu>
2023-02-04 12:13:01 -0800

    Ready to add tests

--- RplTaskTest.cpp
+++ RplTaskTest.cpp
@@ -1,6 +1,5 @@
 #include <gtest/gtest.h>
 #include "RplTask.h"
-#include "TaskLinkedList"
 
 /*tests TODO:
 * - check all statuses are set correctly
@@ -9,3 +8,39 @@
 * - 
 */
 
+
+TEST(statusTest, ) {
+	TaskLinkedList list;
+	RplTask* task1 = (RplTask*)new TestTask();
+
+	list.add(task1);
+	EXPECT_EQ(list.getSize(), 1);
+	EXPECT_EQ(list.head->value, task1);
+	//delete task1;
+	//RplTask* task2 = new RplTask();
+	//list.add(task2);
+}
+
+TEST(addDependTest, AddEmptyTest) {
+	
+}
+
+TEST(checkDependTest, AddEmptyTest) {
+	
+}
+
+TEST(startTest, AddEmptyTest) {
+	
+}
+
+TEST(pauseTest, AddEmptyTest) {
+	
+}
+
+TEST(resumeTest, AddEmptyTest) {
+	
+}
+
+TEST(processTest, AddEmptyTest) {
+	
+}
