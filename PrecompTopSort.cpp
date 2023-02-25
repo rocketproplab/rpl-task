e9851426febb2885af1a695d94305f53ecf65946
Adam Bauer <abauer121@gmail.com>
2023-02-25 13:53:34 -0800

    TopSort implemented
    Implemented the TopSort.

--- PrecompTopSort.cpp
+++ PrecompTopSort.cpp
@@ -2,8 +2,10 @@
 #include <vector>
 #include "RplTask.h"
 #include <string>
-#include <unorder_map>
+#include <unordered_map>
 #include <tuple>
+#include "PrecompTopSort.h"
+#include <unordered_set>
 
 using namespace std;
 
@@ -21,6 +23,7 @@ END-TASK-DEPENDENCIES
 
 
 int main(int argc, char** argv){
+	
 	if(argc == 0){
 		return 1;
 	}
@@ -32,13 +35,15 @@ int main(int argc, char** argv){
 
 	Graph graph;
 
+
 	for(string path : taskPaths){
 		string taskName = getTaskName(path);
 		vector<string> dependencies = getDependencies(path);
 		graph[taskName] = dependencies;
 	}
 
-	vector<string> sorted = topologicalSort(graph);
+vector<string> sorted  = topologicalSort(graph);
+
 
 	outputWithOutputStrategy(sorted);	
 	return 0;
@@ -46,16 +51,58 @@ int main(int argc, char** argv){
 
 vector<string> getDependencies(string path){
 	//TODO	
+	vector<string> dependencies ;
+
+	return dependencies ; 
 }
 
 
-getTaskName(string path){
+string getTaskName(string path){
 	//TODO
+	string taskName = "" ;
+
+	return taskName ; 
 }
 
 
 vector<string> topologicalSort(Graph & g){
 	//TODO	
+
+	vector<string> topologicalOrdering ;
+	unordered_set<string> permanentMarks ; 
+	unordered_set<string> temporaryMarks ; 
+	
+	
+
+	for (auto element: g) {
+		visit(element.first,temporaryMarks,permanentMarks,topologicalOrdering,g) ;
+		}
+
+	return topologicalOrdering ; 
+}
+
+void visit(string task, unordered_set<string> & temporaryMarks, unordered_set<string> & permanentMarks, vector<string> & topologicalOrder, Graph & g ) {
+
+	if (permanentMarks.find(task) != permanentMarks.end() ) {
+		return ; 
+	}
+
+	else if (temporaryMarks.find(task) != temporaryMarks.end()) {
+		throw std::invalid_argument("Graph has cycle");
+	}
+
+	temporaryMarks.insert(task) ;
+
+	for (auto adjacentNodes : g[task]) {
+		visit(adjacentNodes, temporaryMarks, permanentMarks, topologicalOrder, g) ;
+	}
+
+	temporaryMarks.erase(task) ;
+	permanentMarks.insert(task) ;
+
+	topologicalOrder.insert(topologicalOrder.begin(),task) ;
+
+
 }
 
 void outputWithOutputStrategy(vector<string> & sorted){
--- PrecompTopSort.h
+++ PrecompTopSort.h
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <vector>
-#include "RplTask.h"
+//#include "RplTask.h"
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 
-typedef unorder_map<string, vector<string>> Graph;
-vector<string> getDependencies(string path);
-vector<string> topologicalSort(Graph & g);
-string getTaskName(string path);
-void outputWithOutputStrategy(vector<string> & sorted);
+using namespace std ; 
+
+
+
+typedef unordered_map<string, vector<string> > Graph;
+vector<string> getDependencies(string path); // Reads in from the path and outputs the dependencies 
+vector<string> topologicalSort(Graph & g); // Needs to be implemented similarish to old one
+void visit(string task, unordered_set<string> & temporaryMarks, unordered_set<string> & permanentMarks, vector<string> & topologicalOrder, Graph & g ) ;
+string getTaskName(string path); // Retrieve the name of the RPLTask directly from the text file 
+void outputWithOutputStrategy(vector<string> & sorted); // Use the strategy (from Trello board) for the output order
 
