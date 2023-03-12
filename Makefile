bfdb43247203bd11b5a6975da89a2dda447f7a34
Combobyte <varun.sreedhar1@gmail.com>
2023-03-12 11:14:50 -0700

    Made main file for precomptopsort

--- Makefile
+++ Makefile
@@ -8,12 +8,12 @@ clean:
 	rm -r build
 
 precomp:
-	g++ -Wall -pedantic -g -O0 -std=c++11 -o Precomp PrecompTopSort.cpp
+	g++ -Wall -pedantic -g -O0 -std=c++11 -o RplTaskMainBuild RplTaskMainBuild.cpp PrecompTopSort.cpp
 
 precomp_test:
-	./Precomp TestTaskFolder/Task1.h TestTaskFolder/Task2.h TestTaskFolder/Task3.h TestTaskFolder/Task4.h
+	./RplTaskMainBuild TestTaskFolder/Task1.h TestTaskFolder/Task2.h TestTaskFolder/Task3.h TestTaskFolder/Task4.h
 
 precomp_clean: PrecompTopSort.cpp PrecompTopSort.h
-	rm -r Precomp.dSYM
-	rm Precomp
+	rm -r RplTaskMainBuild.dSYM
+	rm RplTaskMainBuild
 	
--- PrecompTopSort.cpp
+++ PrecompTopSort.cpp
@@ -15,48 +15,6 @@ END-TASK-DEPENDENCIES
 END-RPL-TASK-HEADER
 */
 
-/*
-int main(int argc, char** argv){
-	if(argc <= 1){
-		cout << "Wrong number of inputs" << endl;
-		return 1;
-	}
-
-	vector<string> taskPaths;
-	for(int i = 1; i < argc; ++i){
-		taskPaths.push_back(argv[i]);
-	}
-
-	Graph graph;
-	unordered_map<string, Task> taskMap;
-
-	for(string path : taskPaths){
-		string contents=getFileContents(path);
-		string taskName = getTaskName(contents);
-		bool isStart = isTaskStartOnBoot(contents);
-		vector<string> dependencies = getDependencies(contents);
-
-		Task task;
-		task.className = taskName;
-		task.startOnBoot = isStart;
-
-		graph[taskName] = dependencies;
-		taskMap[taskName] = task;
-	}
-
-	vector<string> sorted  = topologicalSort(graph);
-	vector<Task> sortedTasks;
-
-	for(string str : sorted){
-		sortedTasks.push_back(taskMap[str]);
-	}
-
-	//iOutputStrategyPattern pattern;
-	ArduinoStrategyPattern pattern;
-	outputWithOutputStrategy(sortedTasks, &pattern);	
-	return 0;
-}
-*/
 
 string getFileContents(string path){
 	string output;
